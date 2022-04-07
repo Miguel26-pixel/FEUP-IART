f2be15ca91d562a35bab99cee7713eb0c41f300c
Nuno Alves <nyansword@gmail.com>
2022-04-07 08:55:13 +0100

    add: single-point crossover

--- src/utils/crossover.py
+++ src/utils/crossover.py
@@ -24,23 +24,46 @@ import time
 def get_common_junctions(list1, list2):
     return (set(list1) & set(list2))
 
+def get_crossover_point(parent1 : List[int], parent2 : List[int]):
+    common_junctions = get_common_junctions(parent1, parent2)
+
+    chosen_junction = random.choice(tuple(common_junctions))
+    junction_indexes = []
+
+    for idx, junction in enumerate(parent1):
+        if junction == chosen_junction:
+            junction_indexes.append(idx)
+
+    chosen_idx1 = random.choice(junction_indexes)
+    junction_indexes.clear()
+
+    for idx, junction in enumerate(parent2):
+        if junction == chosen_junction:
+            junction_indexes.append(idx)
+
+    chosen_idx2 = random.choice(junction_indexes)
+
+    return (chosen_idx1, chosen_idx2)
+
 def singlepoint_crossover(parent1 : List[int], parent2 : List[int]):
-    return get_common_junctions(parent1, parent2)
+    (idx1, idx2) = get_crossover_point(parent1, parent2)
+
+    return parent2[:idx2] + parent1[idx1:]
 
 
-sum = 0
+# sum = 0
 
-for i in range(0, 100):
-    list1 = [random.randint(0,1000) for i in range(0, random.randint(10000, 100000))]
-    list2 = [random.randint(0,1000) for i in range(0, random.randint(10000, 100000))]
+# for i in range(0, 100):
+#     list1 = [random.randint(0,1000) for i in range(0, random.randint(10000, 100000))]
+#     list2 = [random.randint(0,1000) for i in range(0, random.randint(10000, 100000))]
 
-    start = time.time()
-    get_common_junctions2(list1, list2)
-    end = time.time()
+#     start = time.time()
+#     singlepoint_crossover(list1, list2)
+#     end = time.time()
 
-    sum += (end - start)
+#     sum += (end - start)
 
-print(sum/100)
+# print(sum/100)7
 
 
-# print(singlepoint_crossover([1,2,1], [3,1,1]))
+print(singlepoint_crossover([1,2,3,4,5,6], [6,4,3,5,7,3]))
