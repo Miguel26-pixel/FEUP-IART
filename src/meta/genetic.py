8a0ca17786bcbe98b6e69d31ee1b3c8fbf23725b
Nuno Alves <nyansword@gmail.com>
2022-04-19 15:11:02 +0100

    add: mutate child on random chance

--- src/meta/genetic.py
+++ src/meta/genetic.py
@@ -1,11 +1,10 @@
-from audioop import cross
 from math import floor
 from time import time
 from typing import List
+import random
 from utils.routing import Router
-from utils.genetic import get_initial_pop
-from utils.crossover import SA_crossover
-
+from utils.genetic import get_initial_pop, random_select
+from utils.crossover import SA_crossover, crossover
 
 class GeneticSolver:
     def __init__(self, problem_info: Router, run_time: int):
@@ -17,6 +16,7 @@ class GeneticSolver:
         self._queen_ratio = 0.4
         self._run_time = run_time
         self._crossover_function = SA_crossover
+        self._mutation_chance = 0.2
 
     def set_pop_size(self, pop_size):
         self._pop_size = pop_size
@@ -30,11 +30,30 @@ class GeneticSolver:
     def set_crossover_function(self, crossover_function):
         self._crossover_function = crossover_function
 
-    def genetic_loop(self, population: List[List[int]]):
+    def set_mutation_chance(self, mutation_chance):
+        self._mutation_chance = mutation_chance
+
+    pop_size = property(fset=set_pop_size)
+    min_pop_size = property(fset=set_min_pop_size)
+    queen_ratio = property(fset=set_queen_ratio)
+    crossover_function = property(fset=set_crossover_function)
+    mutation_chance = property(fset=set_mutation_chance)
+
+    def genetic_loop(self, population: List[List[List[int]]]):
         init_time = time()
 
         while(time() - init_time < self._run_time):
-            pass
+            parent1 = random_select(population)
+            parent2 = random_select(population)
+
+            child = crossover(parent1, parent2,
+                              self._problem_info.graph, self._crossover_function)
+
+            random_value = random.uniform(0, 1)
+
+            if random_value < self._mutation_chance:
+                # TODO: mutate this bitch
+                pass
 
     def solve(self):
         n_streets = len(self._problem_info.graph.streets)
@@ -49,8 +68,3 @@ class GeneticSolver:
         print(f"Population generated in {time() - start_time} seconds!")
 
         return self.genetic_loop(population)
-
-    pop_size = property(fset=set_pop_size)
-    min_pop_size = property(fset=set_min_pop_size)
-    queen_ratio = property(fset=set_queen_ratio)
-    crossover_function = property(fset=set_crossover_function)
